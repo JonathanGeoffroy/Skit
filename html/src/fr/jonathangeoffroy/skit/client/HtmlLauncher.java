b9e699a94b31efdd935e68973a3904e7bc7234bb
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2016-01-02 11:19:16 +0100

    Generate The LibGDX Skit project.

---
+++ html/src/fr/jonathangeoffroy/skit/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package fr.jonathangeoffroy.skit.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import fr.jonathangeoffroy.skit.SkitGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SkitGame();
+        }
+}
